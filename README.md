3965b7149d4f3c43658a136e5957094948f47d00
farukfakcin <78468550+farukfakcin@users.noreply.github.com>
2023-07-20 08:50:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-tmxjsx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-tmxjsx)
---
+++ src/doc-page/index.js
@@ -0,0 +1,7 @@
+import Grid from '@mui/material/Grid';
+
+const DocPage = () => {
+  return <Grid item xs={12} md={6} />;
+};
+
+export default DocPage;
